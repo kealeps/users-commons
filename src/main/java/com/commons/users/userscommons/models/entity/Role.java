b8c1a54ada62cbcff6222f6d05b157b3fe892a0f
Kevin Perez <kevinp@cronapis.com>
2021-09-19 14:22:45 -0400

    first commit

---
+++ src/main/java/com/commons/users/userscommons/models/entity/Role.java
@@ -0,0 +1,28 @@
+package com.commons.users.userscommons.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Table(name = "roles")
+@Data
+@ToString
+public class Role implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 30)
+    private String name;
+    
+}
---
+++ src/test/java/com/commons/users/userscommons/UsersCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commons.users.userscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
